eb4faf3ff73d75377bf178db1c8ad4f704c0a088
Narmin <127186131+narminhaciyeva@users.noreply.github.com>
2023-04-19 13:54:47 +0400

    Add files via upload

---
+++ js-praktika 19.04/assets/js/main.js
@@ -0,0 +1,50 @@
+// Ededlerden ibaret array verilib.Arrayin en kicik qiymetini qaytaran funksiya yazin. 
+// var nums=[-2,4,-1,6,2,3,9]
+// function findMin(arr){
+//     var min=arr[0];
+//     for(let i=1; i<arr.length; i++){
+//         if (arr[i]<min){
+//             min=arr[i];
+//         }
+//     }
+//     return min;
+// }
+// console.log("Arrayin en kicik elementi :"+ findMin(nums));
+
+
+
+// Ededlerden ibaret array verilib.Arrayin en boyuk qiymetini qaytaran funksiya yazin
+
+// var nums=[4,2,-1,9,44,78,25,98]
+// function findMax(arr){
+//     var max=arr[0]
+//     for(let i=1; i<arr.length;i++){
+//         if(arr[i]>max){
+//             max=arr[i];
+//         }
+//     }
+//     return max;
+// }
+// console.log("Arrayin en boyuk qiymeti : "+ findMax(nums));
+
+
+// Cut yerde dayanan reqemleri secin 
+
+// var array=[4,3,8,11,34,87,96]
+// function evenNumbers(arr){
+//     for(let i=0; i<arr.length; i++){
+//         if(i%2==0){
+
+//         }
+//     }
+// return evenNumbers;
+// }
+//   YAZAMMADIM
+// console.log(Cut ededler);
+
+
+
+// Verilen ededin 2-nin quvveti olub-olmadigini yoxlayin eger 2-nin quvvetidirse ekrana funksiya yazilsin 
+  
+
+
